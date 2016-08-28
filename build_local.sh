6574aa1eac276d66115beaa5846e07c3bf6274a7
zekaf <jose.faisca@gmail.com>
2016-08-28 16:48:44 +0000

    Update

--- build_local.sh
+++ build_local.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-docker build --tag zekaf/pdns-recursor:v1 .
+#
+# Build local docker image
+#
+
+# change BUILDER
+sed -e 's/DOCKERHUB/LOCAL/g' Dockerfile > Dockerfile.local
+
+#set MAKEJOBS
+procs=$(grep processor /proc/cpuinfo | wc -l)
+procs=$((procs+1))
+sed -i -e 's/\-j3/\-j'${procs}'/g' Dockerfile.local
+
+IMAGE="$(grep IMAGE Dockerfile | awk '{print $3}' )"
+
+# Build image
+docker build -f Dockerfile.local -t ${IMAGE} .
--- run-recursor-dnschain-docker.sh
+++ run-recursor-dnschain-docker.sh
@@ -19,36 +19,29 @@ fi
 
 # Docker image
 IMG="zekaf/pdns-recursor"
-
-# Image tag
 TAG="latest"
+
+# Docker network
+NET_NAME="isolated_nw"
 
 # PowerDNS Recursor container
 PDNSR_CT="pdns-recursor"
-
-# PowerDNS Recursor Port
 PDNSR_PORT="53"
+PDNSR_IP="10.17.0.4"
+
+# DNSChain container
+DNSCHAIN_CT="$1"
+DNSCHAIN_CONF="/etc/dnschain/dnschain.conf"
+DNSCHAIN_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $DNSCHAIN_CT)
+DNSCHAIN_PORT=$(docker exec $DNSCHAIN_CT cat $DNSCHAIN_CONF | grep port | awk -F '[/=]' '{print $2}' | sed '2d' | xargs)
+IP_PORT="${DNSCHAIN_IP}:${DNSCHAIN_PORT}"
 
 # Host port
 HOST_PORT="53"
 
-# DNSChain container
-DNSCHAIN_CT="$1"
-
-# DNSChain configuration
-DNSCHAIN_CONF="/etc/dnschain/dnschain.conf"
-
-# DNSChain IP address
-DNSCHAIN_IP=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' $DNSCHAIN_CT)
-
-# DNSChain DNS port
-DNSCHAIN_PORT=$(docker exec $DNSCHAIN_CT cat $DNSCHAIN_CONF | grep port | awk -F '[/=]' '{print $2}' | sed '2d' | xargs)
-
-# DNSChain IP:port
-IP_PORT="${DNSCHAIN_IP}:${DNSCHAIN_PORT}"
-
 docker run -d -p ${HOST_PORT}:${PDNSR_PORT}/udp \
-	--net netstatic --ip 172.18.0.53 \
+	--net $NET_NAME --ip $PDNSR_IP \
+	--restart=always \
 	--name ${PDNSR_CT} $IMG:$TAG \
 	--forward-zones=bit.=${IP_PORT},dns.=${IP_PORT},eth.=${IP_PORT},p2p.=${IP_PORT} \
 	--export-etc-hosts=no \
